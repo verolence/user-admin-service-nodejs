387b326e6ef36db94d28a532daa422331765824f
verolence <verolence@yandex.ru>
2025-10-02 00:05:51 +0300

    add endpoint to get all users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,23 +1,30 @@
 import express from 'express';
 import { User } from '../models/User';
 
+
 const router = express.Router();
 
-// данные пользователя по ID (только админ или сам пользователь)
+// данные пользователя по ID
 // (req, res) => {} - функция-обработчик
+
 router.get('/:id', async (req, res) => {
   try {
     const userId = req.params.id;
-    const currentUser = (req as any).user;
-
-    if (currentUser.role !== 'admin' && currentUser.userId !== userId) {
-      return res.status(403).json({ message: 'Доступ запрещен' });
-    }
-
-    const user = await User.findById(userId);
+    const user = await User.findById(userId).select('-password');
     if (!user) return res.status(404).json({ message: 'Пользователь не найден' });
 
     res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
+});
+
+// список всех пользователей
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find().select('-password'); // не возвращаем пароль
+    res.json(users);
   } catch (err) {
     res.status(500).json({ message: 'Ошибка сервера' });
   }
